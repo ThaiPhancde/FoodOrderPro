9388837a48c4ac4a02a0fe131cb8e604a44293c3
ThaiAn <anthaiphanxuan@gmail.com>
2024-12-01 21:01:32 +0700

    test

---
+++ app/src/main/java/com/pro/foodorder/constant/AboutUsConfig.java
@@ -0,0 +1,18 @@
+package com.pro.foodorder.constant;
+
+public interface AboutUsConfig {
+    String ABOUT_US_TITLE = "ThaiAn";
+    String ABOUT_US_CONTENT = "Provides Android, iOS, Web Source Codes and Development Services. " +
+            "Highly Committed to Quality Products and Excellent Services.";
+    String ABOUT_US_SLOGAN = "Highly committed to quality products & excellent services";
+    String ABOUT_US_WEBSITE_TITLE = "Website ThaiAn";
+
+    String PAGE_FACEBOOK = "fb://page/61568903451600";
+    String LINK_FACEBOOK = "https://www.facebook.com/Thai.An.249/";
+    String LINK_YOUTUBE = "";
+    String PHONE_NUMBER = "0569083547";
+    String GMAIL = "anthaiphanxuan@gmail.com";
+    String SKYPE_ID = "";
+    String ZALO_LINK = "https://zalo.me/0569083547";
+    String WEBSITE = "https://www.youtube.com/watch?v=xvFZjo5PgG0";
+}
